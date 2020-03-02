43bc944798e35c24c44379b9b26945ea6c4c1fe3
elpiojo95 <leo1ragomez95@gmail.com>
2020-03-02 23:09:19 +0100

    implemented main logic

--- timmyBot_arduino/src/main.cpp
+++ timmyBot_arduino/src/main.cpp
@@ -1,14 +1,18 @@
-
 #include "Arduino.h"
-#include "DRV8835MotorShield.h"
-#include <Kinematics.h>
+#include "timmy.h"
 
-void setup()
-{
+/* Bluethoot comunication */
+SoftwareSerial HC05 (11, 12);  //pin RX, pin TX
 
+void setup() {
+  Serial.begin(9600);        //Inicializa puerto serie por hard
+  HC05.begin(9600);    //Inicializa puerto serie por soft
 }
 
-void loop()
-{
-
+void loop() {
+  String data;
+  float linear, angular;
+  data = ReadData(HC05);
+  ConvertData(data, &linear, &angular);
+  MoveRobot(linear, angular);
 }
