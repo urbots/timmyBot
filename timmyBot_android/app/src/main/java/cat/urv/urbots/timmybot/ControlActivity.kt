25660737c593563c920d936422d8829656f48080
Adam Alvarado <d3ltcod@protonmail.com>
2020-03-02 19:07:12 +0100

    Created basic structure to get bluetooth devices

---
+++ timmyBot_android/app/src/main/java/cat/urv/urbots/timmybot/ControlActivity.kt
@@ -0,0 +1,7 @@
+package cat.urv.urbots.timmybot
+
+import androidx.appcompat.app.AppCompatActivity
+
+class ControlActivity: AppCompatActivity() {
+
+}
--- timmyBot_android/app/src/main/java/cat/urv/urbots/timmybot/MainActivity.kt
+++ timmyBot_android/app/src/main/java/cat/urv/urbots/timmybot/MainActivity.kt
@@ -2,28 +2,78 @@ package cat.urv.urbots.timmybot
 
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
+import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+    private val listDevices : ArrayList<BluetoothDevice> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
         if (bluetoothAdapter == null) {
             Toast.makeText(applicationContext, R.string.bluetooth_not_supported, Toast.LENGTH_LONG).show()
         } else {
-            if (!bluetoothAdapter?.isEnabled) {
+            if (!bluetoothAdapter.isEnabled) {
                 val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            } else {
+                discover()
             }
         }
 
+    }
+
+    // Create a BroadcastReceiver for ACTION_FOUND.
+    private val receiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action: String? = intent.action
+            when(action) {
+                BluetoothDevice.ACTION_FOUND -> {
+                    listDevices.add(intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE))
+                    devicesList()
+                }
+            }
+        }
+    }
+
+    private fun discover() {
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(receiver, filter)
+
+        val pairedDevices: Set<BluetoothDevice> ? = bluetoothAdapter?.bondedDevices
+        pairedDevices?.forEach { device ->
+            listDevices.add(device)
+        }
+
+        devicesList()
+    }
+
+    private fun devicesList() {
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listDevices)
+        select_device_list.adapter = adapter
+        select_device_list.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            val device: BluetoothDevice = listDevices[position]
+            val address: String = device.name + " " + device.address
+
+            val intent = Intent(this, ControlActivity::class.java)
+            intent.putExtra(EXTRA_ADDRESS, address)
+            startActivity(intent)
+        }
     }
 
     override fun onActivityResult(
@@ -35,10 +85,18 @@ class MainActivity : AppCompatActivity() {
         if (resultCode == Activity.RESULT_CANCELED) {
             Toast.makeText(applicationContext, R.string.bluetooth_must_be_enabled, Toast.LENGTH_SHORT).show()
             finish()
+        } else {
+            discover()
         }
     }
 
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(receiver)
+    }
+
     companion object {
-        private val REQUEST_ENABLE_BT = 1
+        private const val REQUEST_ENABLE_BT = 1
+        const val EXTRA_ADDRESS: String = "Device_address"
     }
 }
