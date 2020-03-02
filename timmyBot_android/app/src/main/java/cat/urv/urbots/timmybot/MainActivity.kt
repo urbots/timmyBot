6b30c7da18506889a21ae3f4ee98a224902eade3
Adam Alvarado <d3ltcod@protonmail.com>
2020-03-02 17:32:47 +0100

    Added dialog to activate bluetooth

--- timmyBot_android/app/src/main/java/cat/urv/urbots/timmybot/MainActivity.kt
+++ timmyBot_android/app/src/main/java/cat/urv/urbots/timmybot/MainActivity.kt
@@ -1,12 +1,30 @@
 package cat.urv.urbots.timmybot
 
+import android.bluetooth.BluetoothAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        if (bluetoothAdapter == null) {
+            Toast.makeText(this, R.string.bluetooth_not_supported, Toast.LENGTH_LONG).show()
+        } else {
+            if (!bluetoothAdapter?.isEnabled) {
+                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
+            }
+        }
+
+    }
+
+    companion object {
+        private val REQUEST_ENABLE_BT = 1
     }
 }
