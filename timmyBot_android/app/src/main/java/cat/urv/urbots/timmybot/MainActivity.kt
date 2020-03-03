e2ed07c29b40d0deca4dda205926bd738ace960e
Adam Alvarado <d3ltcod@protonmail.com>
2020-03-03 09:52:26 +0100

    Added request permissions for location

--- timmyBot_android/app/src/main/java/cat/urv/urbots/timmybot/MainActivity.kt
+++ timmyBot_android/app/src/main/java/cat/urv/urbots/timmybot/MainActivity.kt
@@ -1,5 +1,6 @@
 package cat.urv.urbots.timmybot
 
+import android.Manifest
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothDevice
@@ -7,19 +8,20 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Toast
-import androidx.annotation.Nullable
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     private val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
-    private val listDevices : ArrayList<BluetoothDevice> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,14 +30,27 @@ class MainActivity : AppCompatActivity() {
         if (bluetoothAdapter == null) {
             Toast.makeText(applicationContext, R.string.bluetooth_not_supported, Toast.LENGTH_LONG).show()
         } else {
+            reqPermissions()
+
+            // Register for broadcasts when a device is discovered.
+            val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+            registerReceiver(receiver, filter)
+
             if (!bluetoothAdapter.isEnabled) {
                 val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
                 startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-            } else {
-                discover()
             }
+
+            discover()
         }
 
+    }
+
+    private fun reqPermissions() {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), REQUEST_FINE_LOCATION)
+        }
     }
 
     // Create a BroadcastReceiver for ACTION_FOUND.
@@ -44,51 +59,50 @@ class MainActivity : AppCompatActivity() {
             val action: String? = intent.action
             when(action) {
                 BluetoothDevice.ACTION_FOUND -> {
-                    listDevices.add(intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE))
-                    devicesList()
+                    val device: BluetoothDevice =
+                        intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                    val deviceName = device.name
+                    val deviceHardwareAddress = device.address // MAC address
+                    println(deviceName + " " + deviceHardwareAddress)
                 }
             }
         }
     }
 
+
     private fun discover() {
-        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
-        registerReceiver(receiver, filter)
-
-        val pairedDevices: Set<BluetoothDevice> ? = bluetoothAdapter?.bondedDevices
+        bluetoothAdapter?.startDiscovery()
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter?.bondedDevices
         pairedDevices?.forEach { device ->
-            listDevices.add(device)
+            val deviceName = device.name
+            val deviceHardwareAddress = device.address // MAC address
+            println(deviceName + " " + deviceHardwareAddress)
         }
 
-        devicesList()
     }
 
-    private fun devicesList() {
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, listDevices)
-        select_device_list.adapter = adapter
-        select_device_list.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
-            val device: BluetoothDevice = listDevices[position]
-            val address: String = device.name + " " + device.address
-
-            val intent = Intent(this, ControlActivity::class.java)
-            intent.putExtra(EXTRA_ADDRESS, address)
-            startActivity(intent)
-        }
-    }
-
-    override fun onActivityResult(
-        requestCode: Int,
-        resultCode: Int,
-        data: Intent?
-    ) {
+    override fun onActivityResult(requestCode: Int,  resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (resultCode == Activity.RESULT_CANCELED) {
             Toast.makeText(applicationContext, R.string.bluetooth_must_be_enabled, Toast.LENGTH_SHORT).show()
             finish()
-        } else {
-            discover()
         }
     }
+
+    override fun onRequestPermissionsResult(requestCode: Int,
+                                            permissions: Array<String>, grantResults: IntArray) {
+        when (requestCode) {
+            REQUEST_FINE_LOCATION -> {
+                // If request is cancelled, the result arrays are empty.
+                if (!(grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
+                    Toast.makeText(applicationContext, R.string.permissions_are_required, Toast.LENGTH_LONG).show()
+                    finish()
+                }
+                return
+            }
+        }
+    }
+
 
     override fun onDestroy() {
         super.onDestroy()
@@ -97,6 +111,6 @@ class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val REQUEST_ENABLE_BT = 1
-        const val EXTRA_ADDRESS: String = "Device_address"
+        private const val REQUEST_FINE_LOCATION = 2
     }
 }
